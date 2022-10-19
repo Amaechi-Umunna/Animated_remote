1b8dfc421d4c08c953111823c468a7f88fedc318
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-10-19 11:31:11 +0100

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Amaechi Chukwu on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
